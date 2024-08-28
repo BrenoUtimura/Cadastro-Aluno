a265bcd8e9413e051e2485aa96d7c249fddb9ea6
Breno Hideki Utimura <94381719+BrenoUtimura@users.noreply.github.com>
2024-08-28 13:29:26 -0300

    Upload alunos

--- alunos.c
+++ alunos.c
@@ -4,27 +4,27 @@ typedef struct{
     char nome[50];
     int rga;
     float n1, n2, n3, ma;
-    int result;
 }regaluno;
 
-void cadastroAluno(int n);
-void mostrandoValores(int n);
+void cadastroAluno(regaluno alunos[], int n);
+void mostrandoValores(regaluno alunos[], int n);
+int buscaAluno(regaluno alunos[], int num, int n);
 
 int main(void){
     
     regaluno alunos[50];
-    int n, i, j, flag, num, resp;
+    int n, i, flag, num, resp, posicao;
     
     flag = 1;
     
     while(flag == 1){
         printf("O que vc deseja fazer agora? \n ");
         printf("------------------------\n");
-        printf("1 - Cadastrar um novo Aluno \n"); /* Feito */
-        printf("2 - Remover Aluno \n"); /**/
-        printf("3 - Atualizar as notas dos Alunos \n"); /**/
-        printf("4 - Exibir Alunos cadastrados \n"); /* Feito */
-        printf("5 - Exibir as medias dos Alunos \n"); /**/
+        printf("1 - Cadastrar um novo Aluno \n"); 
+        printf("2 - Remover Aluno \n"); 
+        printf("3 - Atualizar as notas dos Alunos \n"); 
+        printf("4 - Exibir Alunos cadastrados \n"); 
+        printf("5 - Exibir as medias dos Alunos \n"); 
         printf("7 - Sair \n");
         printf("------------------------\n");
         
@@ -41,7 +41,7 @@ int main(void){
                     printf("Quantos Alunos vc ira cadastrar?  ");
                     scanf("%d", &n);
 
-                    cadastroAluno(n);
+                    cadastroAluno(alunos, n);
 
                     printf("Informe um numero para poder continuar: (1 sim ou 0 nao)\n ");
                     scanf("%d", &resp);
@@ -54,18 +54,18 @@ int main(void){
                     printf("Informe o rga que deseja remover:  ");
                     scanf("%d", &num);
                     
-                    i = 0;
-                    while(i < n){
-                        
-                        if(num == alunos[i].rga){
-                            for(j = i; j < n; j++){
-                                alunos[i].rga = alunos[i + 1].rga;
-                            }
-                            i = i + n;
+                    posicao = buscaAluno(alunos, num, n);
+
+                    if(posicao == -1)
+                        printf("RGA não encontrado!!\n");
+                    else{
+                        for(i = posicao; i < n - 1; i++){
+                           alunos[i] = alunos[i + 1];  
                         }
-                        
-                        i = i + 1;
-                    }
+                        n = n - 1;
+
+                        printf("\nALUNO REMOVIDO COM SUCESSO!! \n\n");
+                    } 
 
                     printf("Informe um numero para poder continuar: (1 sim ou 0 nao)\n ");
                     scanf("%d", &resp);
@@ -75,8 +75,26 @@ int main(void){
                 break;
                 
                 case 3:
-                    printf("Informe o rga:  ");
+                    printf("Informe o rga para atualizar as notas:  ");
                     scanf("%d", &num);
+
+                    posicao = buscaAluno(alunos, num, n);
+
+                    if(posicao == -1)
+                        printf("RGA não encontrado!! \n");
+                    else{
+                        printf("Informe o valor da primeira nota:  ");
+                        scanf("%f", &alunos[posicao].n1);
+
+                        printf("Informe o valor da segunda nota:  ");
+                        scanf("%f", &alunos[posicao].n2);
+
+                        printf("Informe o valor da terceira nota:  ");
+                        scanf("%f", &alunos[posicao].n3);
+
+
+                        printf("\nNOTAS ATUALIZADAS COM SUCESSO!! \n\n");
+                    }
 
                     printf("Informe um numero para poder continuar: (1 sim ou 0 nao)\n ");
                     scanf("%d", &resp);
@@ -86,7 +104,7 @@ int main(void){
                 break;
                 
                 case 4:
-                    mostrandoValores(n);
+                    mostrandoValores(alunos, n);
 
                     printf("Informe um numero para poder continuar: (1 sim ou 0 nao)\n ");
                     scanf("%d", &resp);
@@ -116,9 +134,8 @@ int main(void){
 
 /* Funções do sistema */
 
-void cadastroAluno(int n){
-
-    regaluno alunos[50];
+void cadastroAluno(regaluno alunos[], int n){
+    
     int i;
                     
     for(i = 0; i < n; i++){
@@ -140,22 +157,45 @@ void cadastroAluno(int n){
        
     }
                     
-    printf("CADASTRADO COM SUCESSO!!\n");
+    printf("\nCADASTRADO COM SUCESSO!! \n\n");
 }
 
 
 
 
-void mostrandoValores(int n){
+void mostrandoValores(regaluno alunos[], int n){
 
-    regaluno alunos[50];
     int i;
 
     for(i = 0; i < n; i++){
-        printf("%s \n", alunos[i].nome);
-        printf("%d \n", alunos[i].rga);
-        printf("%.2f \n", alunos[i].n1);
-        printf("%.2f \n", alunos[i].n2);
-        printf("%.2f \n", alunos[i].n3);
+        printf("Nome: %s \n", alunos[i].nome);
+        printf("RGA:  %d \n", alunos[i].rga);
+        printf("Nota 1:  %.2f \n", alunos[i].n1);
+        printf("Nota 2:  %.2f \n", alunos[i].n2);
+        printf("Nota 3:  %.2f \n", alunos[i].n3);
+        alunos[i].ma = (alunos[i].n1 + alunos[i].n2 + alunos[i].n3) / 3;
+        printf("Media do Aluno:  %.2f \n", alunos[i].ma);
     }
+}
+
+
+
+int buscaAluno(regaluno alunos[], int num, int n){
+
+    int i, posicao;
+
+    i = 0;
+    while(i < n){
+        if(alunos[i].rga == num){
+            posicao = i;
+            i = i + n;
+        }
+        else{
+            posicao = -1;
+        }
+        i = i + 1;
+    }
+
+
+    return posicao;
 }
